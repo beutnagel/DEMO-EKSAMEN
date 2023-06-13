4762bc5b4d401d8b9c193a9869bea510683c2755
Jarne W. Beutnagel <dhreugh@gmail.com>
2023-06-13 17:35:24 +0200

    TODO: add comments to code?

--- DEMO-EKSAMEN/script3.js
+++ DEMO-EKSAMEN/script3.js
@@ -1,50 +1,41 @@
 fetch("errorcode.json")
-  .then(response => {
+  .then((response) => {
     if (!response.ok) {
       throw new Error(`HTTP error: ${response.status}`);
     }
     return response.json();
   })
 
-
-  .then(data => {
+  .then((data) => {
     // Handle the loaded JSON data here
     const errorCodes = data.errorcode;
     console.log(errorCodes);
 
     // Set the data in the DOM
-    const pAlertTekst = document.querySelector('.alert-tekst');
+    const pAlertTekst = document.querySelector(".alert-tekst");
     const randomNumber = Math.floor(Math.random() * errorCodes.length);
     const clarification = errorCodes[randomNumber].clarification;
     pAlertTekst.textContent = clarification;
     console.log(randomNumber, pAlertTekst.textContent);
 
-
+    /* TODO: please write some comment about what this code is meant to do.... */
     const statusAlert = document.querySelector(".headline--alert-tekst");
-    statusAlert.textContent= ` ${errorCodes[randomNumber].status}`;
+    statusAlert.textContent = ` ${errorCodes[randomNumber].status}`;
 
     const warningCircle = document.querySelector(".circle");
     const colorCode = errorCodes[randomNumber].color;
-    console.log(colorCode)
-    
+    console.log(colorCode);
 
-  if (colorCode === "okColor"){
-    warningCircle.classList.add("okColor");
-  }
-  else if( colorCode === "warningColor"){
-    warningCircle.classList.add("warningColor");
-  }
- 
-  else if(colorCode === "alarmColor"){
-    warningCircle.classList.add("alarmColor")
-  }
- } 
+    /* TODO: please write some comment about what this code is meant to do.... */
+    if (colorCode === "okColor") {
+      warningCircle.classList.add("okColor");
+    } else if (colorCode === "warningColor") {
+      warningCircle.classList.add("warningColor");
+    } else if (colorCode === "alarmColor") {
+      warningCircle.classList.add("alarmColor");
+    }
+  })
 
- )
-
-
-  .catch(error => {
+  .catch((error) => {
     console.error("Could not fetch JSON:", error);
   });
-
-
